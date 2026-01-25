4af582550cf9b2ff101bc1699f2c3fcc01b3a8d8
alebal123bal <balzanalessandro2001@gmail.com>
2026-01-25 18:11:16 +0100

    Made paths relative

--- example/luckfox_pico_rtsp_yolov5_UAV/utility_cmds/build_and_load.sh
+++ example/luckfox_pico_rtsp_yolov5_UAV/utility_cmds/build_and_load.sh
@@ -1,5 +1,10 @@
-bash /home/alebalzan/luckfox/luckfox_pico_rkmpi_example/build.sh
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+ROOT_DIR=$(cd "${SCRIPT_DIR}/../../.." && pwd)
+
+bash "${ROOT_DIR}/build.sh"
 
 sshpass -p 'luckfox' scp -r \
-"/home/alebalzan/luckfox/luckfox_pico_rkmpi_example/install/uclibc/luckfox_pico_rtsp_yolov5_UAV_demo" \
+"${ROOT_DIR}/install/uclibc/luckfox_pico_rtsp_yolov5_UAV_demo" \
 "root@172.32.0.93:/root/"
