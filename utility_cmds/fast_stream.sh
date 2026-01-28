e401780128fdba9cd0ead4c5c93b398cf1746e20
alebal123bal <balzanalessandro2001@gmail.com>
2026-01-28 21:40:17 +0100

    Improve window position spawning

--- utility_cmds/fast_stream.sh
+++ utility_cmds/fast_stream.sh
@@ -1,2 +1,2 @@
 gst-launch-1.0 rtspsrc location=rtsp://172.32.0.93/live/0 latency=0 ! \
-  rtph264depay ! avdec_h264 ! fpsdisplaysink sync=false text-overlay=true
+    rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! ximagesink sync=false
