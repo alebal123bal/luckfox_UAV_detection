d682d5fa04a8b0025d3b6bc323a481fb3998092b
alebal123bal <balzanalessandro2001@gmail.com>
2026-01-28 21:45:19 +0100

    Add fps meter

--- utility_cmds/fast_stream.sh
+++ utility_cmds/fast_stream.sh
@@ -1,2 +1,3 @@
 gst-launch-1.0 rtspsrc location=rtsp://172.32.0.93/live/0 latency=0 ! \
-    rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! ximagesink sync=false
+    rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! \
+    fpsdisplaysink video-sink=ximagesink text-overlay=true sync=false
