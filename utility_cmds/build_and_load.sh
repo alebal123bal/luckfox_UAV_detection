7fd2ba7f32cc9e145859c752fed461248514870a
alebal123bal <balzanalessandro2001@gmail.com>
2026-01-31 16:58:04 +0100

    Refactor SCP command to ignore ssh keys

--- utility_cmds/build_and_load.sh
+++ utility_cmds/build_and_load.sh
@@ -5,6 +5,8 @@ ROOT_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 
 bash "${ROOT_DIR}/build.sh"
 
-sshpass -p 'luckfox' scp -r \
-"${ROOT_DIR}/install/uclibc/luckfox_pico_rtsp_yolov5_UAV_demo" \
-"root@172.32.0.93:/root/"
+sshpass -p 'luckfox' scp \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    -r "${ROOT_DIR}/install/uclibc/luckfox_pico_rtsp_yolov5_UAV_demo" \
+    "root@172.32.0.93:/root/"
