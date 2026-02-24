fbb7d4aa242bc77dcf7f6cb15afa9f3de01a3fa3
alebal123bal <balzanalessandro2001@gmail.com>
2026-02-24 22:34:27 +0100

    Add chmod; call other building script

--- utility_cmds/build_and_load.sh
+++ utility_cmds/build_and_load.sh
@@ -4,6 +4,11 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ROOT_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 
 bash "${ROOT_DIR}/build.sh"
+bash "${SCRIPT_DIR}/build_read_detections.sh" uclibc
+
+# Make all built binaries executable
+chmod a+x "${ROOT_DIR}/install/uclibc/luckfox_pico_rtsp_yolov5_UAV_demo/luckfox_pico_rtsp_yolov5_UAV"
+chmod a+x "${ROOT_DIR}/install/uclibc/luckfox_pico_rtsp_yolov5_UAV_demo/utilities/read_detections"
 
 sshpass -p 'luckfox' scp \
     -o StrictHostKeyChecking=no \
